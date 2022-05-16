4c86fa43745bc8eb04dbf9b77dac0527813ad4aa
wjknnn <wjknn3123@gmail.com>
2022-05-16 18:35:31 +0900

    One Scroll Done

--- js/scrollmagic.js
+++ js/scrollmagic.js
@@ -1,15 +1,9 @@
 var controller = new ScrollMagic.Controller();
+var tween = new TimelineMax();
+tween.to("#animation1", 0.3, {opacity: 1}).to("#animation1", 0.3, {opacity: 0}, "+=1");
 
-var tween = TweenMax.to("#animation1", 0.3, {opacity: 1, ease: Linear.easeNone})
-
-new ScrollMagic.Scene({triggerElement: "#animation1", duration: 1500, triggerHook: 0.2})
+new ScrollMagic.Scene({triggerElement: "#animation1", duration: 2500, triggerHook: 0.2})
     .setTween(tween)
     .setPin('#animation1')
     .addIndicators() // add indicators (requires plugin)
     .addTo(controller);
-
-// new ScrollMagic.Scene({triggerElement: "#animation1", duration: 800})
-//     .setTween(tween)
-//     .setPin('#animation1')
-//     .addIndicators() // add indicators (requires plugin)
-//     .addTo(controller);
