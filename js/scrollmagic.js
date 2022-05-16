25eab358605d89d7495374932e31b35fce3ba8ee
wjknnn <wjknn3123@gmail.com>
2022-05-16 12:32:41 +0900

    Egg Edit

--- js/scrollmagic.js
+++ js/scrollmagic.js
@@ -1,8 +1,8 @@
 var controller = new ScrollMagic.Controller();
 
-var tween = TweenMax.to("#animation1", 1.5, {opacity: 1}, {opacity: 0});
+var tween = TweenMax.to("#animation1", 0.3, {opacity: 1, ease: Linear.easeNone})
 
-new ScrollMagic.Scene({triggerElement: "#animation1", duration: 800})
+new ScrollMagic.Scene({triggerElement: "#animation1", duration: 1500, triggerHook: 0.2})
     .setTween(tween)
     .setPin('#animation1')
     .addIndicators() // add indicators (requires plugin)
