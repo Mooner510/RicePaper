72991186e632ca98b9782b1ef7adc1f4bef10cdb
wjknnn <wjknn3123@gmail.com>
2022-05-18 09:32:46 +0900

    Add shadow - Indicators Visible

--- js/scrollmagic.js
+++ js/scrollmagic.js
@@ -8,7 +8,7 @@ tween.to("#animation1", 0.3, {ease: Linear.easeNone, opacity: 1}).to("#animation
 new ScrollMagic.Scene({triggerElement: "#animation1", duration: 2500, triggerHook: 0.2})
     .setTween(timeline)
     .setPin('#animation1')
-    .addIndicators() // add indicators (requires plugin)
+    // .addIndicators() // add indicators (requires plugin)
     .addTo(controller);
 var tweenmove = TweenMax.fromTo("#animation1", 2, {ease: SlowMo.ease.config(0.7, 0.7, false), y: 50}, {ease: SlowMo.ease.config(0.7, 0.7, false), y: -50});
 
@@ -23,7 +23,7 @@ tween2.to("#animation2", 0.3, {ease: Linear.easeNone, opacity: 1}).to("#animatio
 new ScrollMagic.Scene({triggerElement: "#animation2", duration: 2500, triggerHook: 0.3})
     .setTween(timeline2)
     .setPin('#animation2')
-    .addIndicators()
+    // .addIndicators()
     .addTo(controller);
 var tweenmove2 = TweenMax.fromTo("#animation2", 2, {ease: SlowMo.ease.config(0.7, 0.7, false), y: 50}, {ease: SlowMo.ease.config(0.7, 0.7, false), y: -50});
 
@@ -38,7 +38,7 @@ tween3.to("#animation3", 0.3, {ease: Linear.easeNone, opacity: 1}).to("#animatio
 new ScrollMagic.Scene({triggerElement: "#animation3", duration: 2500, triggerHook: 0.3})
     .setTween(timeline3)
     .setPin('#animation3')
-    .addIndicators()
+    // .addIndicators()
     .addTo(controller);
 var tweenmove3 = TweenMax.fromTo("#animation3", 2, {ease: SlowMo.ease.config(0.7, 0.7, false), y: 50}, {ease: SlowMo.ease.config(0.7, 0.7, false), y: -50});
 
